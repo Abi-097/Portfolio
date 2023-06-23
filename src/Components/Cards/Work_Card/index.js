ce0fbd793b7e3ec97b57ca92a496b1d873687c98
Abi-097 <abimahen0312@gmail.com>
2023-06-23 10:12:58 +0530

    work done

--- src/Components/Cards/Work_Card/index.js
+++ src/Components/Cards/Work_Card/index.js
@@ -5,7 +5,6 @@ import { styled } from '@mui/system';
 
 const StyledCard = styled(Card)(({ theme }) => ({
   boxSizing: 'border-box',
-  // maxWidth:'390px',
   width: '100%',
   height: '100%',
   background: 'transparent',
